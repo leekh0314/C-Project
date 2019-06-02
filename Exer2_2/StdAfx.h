b30db04ef34d31ce29f627c5ca245051e6d23720
Lee Echo <36832801+leekh0314@users.noreply.github.com>
2019-06-02 14:42:56 +0800

    Add files via upload

---
+++ Exer2_2/StdAfx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__F4AFEF69_BC44_423A_9526_1DC561596AB4__INCLUDED_)
+#define AFX_STDAFX_H__F4AFEF69_BC44_423A_9526_1DC561596AB4__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__F4AFEF69_BC44_423A_9526_1DC561596AB4__INCLUDED_)
